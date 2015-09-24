1c195beafd4c013f2e07811eeae733f43ebfa137
Bob Lauer <rlauer@gmail.com>
2015-09-24 13:44:08 -0500

    Add support for static methods

--- test/html5-entities.test.js
+++ test/html5-entities.test.js
@@ -12,6 +12,15 @@ describe('html5 entities', function () {
         html5Entities.encodeNonUTF('<>"&©∆').should.equal('&lt;&gt;&quot;&amp;&copy;&#8710;');
         html5Entities.encodeNonASCII('').should.equal('');
         html5Entities.encodeNonASCII('<>"&©®∆').should.equal('<>"&©®&#8710;');
+
+        htmlEntities.Html5Entities.encode('').should.equal('');
+        htmlEntities.Html5Entities.encode('<>"&').should.equal('&lt;&gt;&quot;&amp;');
+        htmlEntities.Html5Entities.encode('<>"&©').should.equal('&lt;&gt;&quot;&amp;&copy;');
+        htmlEntities.Html5Entities.encode('∾̳').should.equal('&acE;');
+        htmlEntities.Html5Entities.encodeNonUTF('').should.equal('');
+        htmlEntities.Html5Entities.encodeNonUTF('<>"&©∆').should.equal('&lt;&gt;&quot;&amp;&copy;&#8710;');
+        htmlEntities.Html5Entities.encodeNonASCII('').should.equal('');
+        htmlEntities.Html5Entities.encodeNonASCII('<>"&©®∆').should.equal('<>"&©®&#8710;');
     });
     it('should decode html5 entities', function () {
         html5Entities.decode('').should.equal('');
@@ -23,5 +32,15 @@ describe('html5 entities', function () {
         html5Entities.decode('&#60;&#x3C;&Aacute;&asdasd;').should.equal('<<Á&asdasd;');
         html5Entities.decode('&acE;').should.equal('∾̳');
         html5Entities.decode('&acE;x').should.equal('∾̳x');
+
+        htmlEntities.Html5Entities.decode('').should.equal('');
+        htmlEntities.Html5Entities.decode('&Lt;&gt;&quot;&amp;').should.equal('≪>"&');
+        htmlEntities.Html5Entities.decode('&lt;&gt;&quot;&amp;').should.equal('<>"&');
+        htmlEntities.Html5Entities.decode('&LT;&GT;&QUOT;&AMP;').should.equal('<>"&');
+        htmlEntities.Html5Entities.decode('&lt;&gt;&quot;&amp;©').should.equal('<>"&©');
+        htmlEntities.Html5Entities.decode('&lt;&gt;&quot;&amp;&copy;').should.equal('<>"&©');
+        htmlEntities.Html5Entities.decode('&#60;&#x3C;&Aacute;&asdasd;').should.equal('<<Á&asdasd;');
+        htmlEntities.Html5Entities.decode('&acE;').should.equal('∾̳');
+        htmlEntities.Html5Entities.decode('&acE;x').should.equal('∾̳x');
     });
 });
--- test/xml-entities.test.js
+++ test/xml-entities.test.js
@@ -11,6 +11,14 @@ describe('xml entities', function () {
         xmlEntities.encodeNonUTF('<>"&©®').should.equal('&lt;&gt;&quot;&amp;&#169;&#174;');
         xmlEntities.encodeNonASCII('').should.equal('');
         xmlEntities.encodeNonASCII('<>"&©®').should.equal('<>"&©®');
+
+        htmlEntities.XmlEntities.encode('').should.equal('');
+        htmlEntities.XmlEntities.encode('<>"&\'').should.equal('&lt;&gt;&quot;&amp;&apos;');
+        htmlEntities.XmlEntities.encode('<>"&©').should.equal('&lt;&gt;&quot;&amp;©');
+        htmlEntities.XmlEntities.encodeNonUTF('').should.equal('');
+        htmlEntities.XmlEntities.encodeNonUTF('<>"&©®').should.equal('&lt;&gt;&quot;&amp;&#169;&#174;');
+        htmlEntities.XmlEntities.encodeNonASCII('').should.equal('');
+        htmlEntities.XmlEntities.encodeNonASCII('<>"&©®').should.equal('<>"&©®');
     });
     it('should decode xml entities', function () {
         xmlEntities.decode('').should.equal('');
@@ -18,5 +26,11 @@ describe('xml entities', function () {
         xmlEntities.decode('&lt;&gt;&quot;&amp;©').should.equal('<>"&©');
         xmlEntities.decode('&lt;&gt;&quot;&amp;©').should.equal('<>"&©');
         xmlEntities.decode('&lt;&gt;&quot;&amp;&copy;&#8710;').should.equal('<>"&&copy;∆');
+
+        htmlEntities.XmlEntities.decode('').should.equal('');
+        htmlEntities.XmlEntities.decode('&lt;&gt;&quot;&amp;&apos;').should.equal('<>"&\'');
+        htmlEntities.XmlEntities.decode('&lt;&gt;&quot;&amp;©').should.equal('<>"&©');
+        htmlEntities.XmlEntities.decode('&lt;&gt;&quot;&amp;©').should.equal('<>"&©');
+        htmlEntities.XmlEntities.decode('&lt;&gt;&quot;&amp;&copy;&#8710;').should.equal('<>"&&copy;∆');
     });
 });
